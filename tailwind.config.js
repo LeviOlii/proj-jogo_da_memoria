693f92178d69feed6d7ca430e2332933390cc2ad
Eduarda Alves <alves.maria.es@gmail.com>
2024-12-14 18:41:29 -0300

    mudanças na fonte e no fundo de página

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
     "./index.html",
     "./js/**/*.js",
   ],
-  theme: {
-    extend: {},
+  theme: { 
+    fontFamily: {
+      "custom": ['"Press Start 2P"', "sans-serif"]
+    },
+    extend: {
+      backgroundImage: {
+        'mario-pattern': "url('/media/bg/pagePattern.jpg')",
+      }
+    },
   },
   plugins: [],
 }
