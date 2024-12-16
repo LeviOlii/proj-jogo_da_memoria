d63a230a28e08d9c636c363ea423aaf1d169d30f
carlosjeferson <carlosjjs500214@gmail.com>
2024-12-16 20:06:03 -0300

    Responsividade mobile e interação botão

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-
 const startButton = document.getElementById('start-button');
-const menu = document.getElementById('menu');
+const menu = document.getElementById('container-menu');
+const cartas = document.getElementById('container-cartas')
 
-startButton.addEventListener('click', function(){
+startButton.addEventListener('click', ()=>{
     menu.classList.add('hidden');
+    cartas.classList.remove('hidden');
+    cartas.classList.add('flex');
 })
